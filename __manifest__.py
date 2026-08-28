199dc47f4e1e346695594021b44a7cb4cc4bb97a
mansi mehta <mansimehta05955@gmail.com>
2026-08-28 18:16:05 +0530

    Add participant and registration models

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Event Management',
+    'version': '1.0',
+    'category': 'Events',
+    'description': """
+        Manage events, participants, venues and registrations.
+    """,
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
